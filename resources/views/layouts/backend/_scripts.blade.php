0b55299f46b4d37d4c6f851ad722f5978cd23310
nayanchakrabarty <nayandcc5@gmail.com>
2019-09-12 09:15:03 +0600

    backend templating is done to staring crud

---
+++ resources/views/layouts/backend/_scripts.blade.php
@@ -0,0 +1,23 @@
+
+<!-- ============================================================== -->
+<!-- End Wrapper -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- All Jquery -->
+<!-- ============================================================== -->
+<script src="{{ asset('backend/assets/libs/jquery/dist/jquery.min.js') }}"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="{{ asset('backend/assets/libs/popper.js/dist/umd/popper.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('backend/dist/js/app-style-switcher.js') }}"></script>
+<!--Wave Effects -->
+<script src="{{ asset('backend/dist/js/waves.js') }}"></script>
+<!--Menu sidebar -->
+<script src="{{ asset('backend/dist/js/sidebarmenu.js') }}"></script>
+<!--Custom JavaScript -->
+<script src="{{ asset('backend/dist/js/custom.js') }}"></script>
+<!--This page JavaScript -->
+<!--chartis chart-->
+<script src="{{ asset('backend/assets/libs/chartist/dist/chartist.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js') }}"></script>
+<script src="{{ asset('backend/dist/js/pages/dashboards/dashboard1.js') }}"></script>
